239caa1da6ae8b4ae191b9f393dfd5c1705505bd
shdnc <a@a.com>
2023-04-25 12:06:38 +0900

    fghjkl;

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# def main():
+st.title("Hello world test!")
+st.header("This is header")
+st.subheader("This is subheader")
+st.text("This is subheader")
+st.write("Streamlit study course")
+#
+# if __name__ == '__main__':
+#     main()
